a2c80bf426ad4c4f91c5511d67a95646eb5293a2
Shoghhh <shoghgaloyan@gmail.com>
2025-02-06 12:04:29 +0400

    Todos screen logic implementation done

--- app/(tabs)/todos/index.tsx
+++ app/(tabs)/todos/index.tsx
@@ -8,23 +8,23 @@ import colors from '@/styles/colors';
 import globalStyles from '@/styles/globalStyles';
 import globalTextStyles from '@/styles/globalTextStyles';
 import { Todo } from '@/types';
-import { Sort, SortLabels } from '@/utils/constants';
+import { Date as myDate, DateLabels, Sort, SortLabels } from '@/utils/constants';
 import { useLocalSearchParams, useRouter } from 'expo-router';
 import moment from 'moment';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { View, Text, TouchableOpacity, FlatList, StyleSheet } from 'react-native';
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
 import { Bar } from 'react-native-progress';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 
-const TodoList = () => {
+export default function TodoList() {
     const router = useRouter();
     const dispatch = useDispatch<AppDispatch>();
     const { date } = useLocalSearchParams();
     const initialDate = Array.isArray(date) ? date[0] : date;
-    console.log({initialDate})
-    const parsedDate = initialDate ? new Date(initialDate) : new Date();
+    const parsedDate = initialDate ? new Date(initialDate) : new Date()
+
     const todos = useSelector((state: RootState) => state.todos.todos);
     // const loading = useSelector((state: RootState) => state.todos.loading);
     // const error = useSelector((state: RootState) => state.todos.error);
@@ -33,30 +33,32 @@ const TodoList = () => {
     const [selectedTab, setSelectedTab] = useState<'incomplete' | 'completed'>('incomplete');
     const [completionProgress, setCompletionProgress] = useState<number>(0);
     const [currentDate, setCurrentDate] = useState(parsedDate);
-    const [sortOption, setSortOption] = useState(Sort.CreatedDate);
-    const sortOptions = [SortLabels[Sort.CreatedDate], SortLabels[Sort.CompletedDate], SortLabels[Sort.Priority]];
+    const [sortOption, setSortOption] = useState(Sort.AssignedDate);
+    const [dateOption, setDateOption] = useState(myDate.ByDay);
+    const dateOptions = Object.entries(DateLabels).map(([key, label]) => ({
+        value: Number(key) as myDate,
+        label,
+    }))
     const [isAscending, setIsAscending] = useState(true);
     const [progressBarWidth, setProgressBarWidth] = useState<number>(0);
     const [viewMode, setViewMode] = useState<'list' | 'tabbed'>('list');
 
-    useEffect(() => {
-        // dispatch(fetchTodos());
-    }, [dispatch]);
+    const sortOptions = useMemo(() => {
+        const options = Object.entries(SortLabels).map(([key, label]) => ({
+            value: Number(key) as Sort,
+            label,
+        }))
+        if (dateOption != myDate.ByDay) {
+            setSortOption(Sort.AssignedDate)
+            return options.filter(el => el.value == Sort.AssignedDate)
+        }
+        setSortOption(Sort.CreatedDate)
+        if (viewMode == 'tabbed') {
+            return options.filter(el => el.value != Sort.AssignedDate && el.value != Sort.CompletedDate)
+        }
 
-    useEffect(() => {
-        const totalTodos = groupTodosByDay(todos, currentDate);
-        const completedTodos = totalTodos.filter((todo) => todo.completed).length;
-        const totalTodosNumber = totalTodos.length;
-        setCompletionProgress(totalTodosNumber ? (completedTodos / totalTodosNumber) * 100 : 0);
-    }, [todos, currentDate]);
-
-    useEffect(() => {
-        sortTodos(sortOption);
-    }, [sortOption, isAscending]);
-
-    const groupTodosByDay = (todos: Todo[], date: Date) => {
-        return todos.filter((todo) => moment(todo.assignedDate).isSame(date, 'day'));
-    };
+        return options.filter(el => el.value != Sort.AssignedDate)
+    }, [dateOption, viewMode]);
 
     const handleLayout = (event: any) => {
         setProgressBarWidth(event.nativeEvent.layout.width);
@@ -67,35 +69,36 @@ const TodoList = () => {
         setDatePickerVisibility(false);
     };
 
-    const goToPreviousDay = () => {
-        setCurrentDate(new Date(currentDate.setDate(currentDate.getDate() - 1)));
+    const changeDate = (n: number) => {
+        setCurrentDate((prev) => {
+            const newDate = new Date(prev);
+            if (dateOption === myDate.ByDay) {
+                newDate.setDate(newDate.getDate() + n);
+            } else if (dateOption === myDate.ByMonth) {
+                newDate.setMonth(newDate.getMonth() + n);
+            } else if (dateOption === myDate.ByYear) {
+                newDate.setFullYear(newDate.getFullYear() + n);
+            }
+            return newDate;
+        });
     };
 
-    const goToNextDay = () => {
-        setCurrentDate(new Date(currentDate.setDate(currentDate.getDate() + 1)));
-    };
-
-    const sortTodos = (option: number) => {
-        const sortedTodos = [...todos];
-        const sortFunc = getSortFunction(option);
-        sortedTodos.sort(sortFunc);
-        dispatch(updateTodoList(sortedTodos));
-    };
-
-    const getSortFunction = (option: number) => {
-        if (option === 0) {
-            return (a: Todo, b: Todo) => (isAscending ? new Date(a.createdDate).getTime() - new Date(b.createdDate).getTime() : new Date(b.createdDate).getTime() - new Date(a.createdDate).getTime());
+    const getSortFunction = (sortOption?: number) => {
+        switch (sortOption) {
+            case Sort.CreatedDate:
+                return (a: Todo, b: Todo) => (isAscending ? new Date(a.createdDate).getTime() - new Date(b.createdDate).getTime() : new Date(b.createdDate).getTime() - new Date(a.createdDate).getTime());
+            case Sort.CompletedDate:
+                return (a: Todo, b: Todo) => {
+                    const aCompletedDate = a.completedDate ? new Date(a.completedDate).getTime() : Infinity;
+                    const bCompletedDate = b.completedDate ? new Date(b.completedDate).getTime() : Infinity;
+                    return isAscending ? aCompletedDate - bCompletedDate : bCompletedDate - aCompletedDate;
+                };
+            case Sort.Priority:
+                return (a: Todo, b: Todo) => (isAscending ? a.priority - b.priority : b.priority - a.priority);
+            case Sort.AssignedDate:
+                return (a: Todo, b: Todo) => isAscending ? new Date(a.assignedDate).getTime() - new Date(b.assignedDate).getTime() : new Date(b.assignedDate).getTime() - new Date(a.assignedDate).getTime()
         }
-        if (option === 1) {
-            return (a: Todo, b: Todo) => {
-                const aCompletedDate = a.completedDate ? new Date(a.completedDate).getTime() : Infinity;
-                const bCompletedDate = b.completedDate ? new Date(b.completedDate).getTime() : Infinity;
-                return isAscending ? aCompletedDate - bCompletedDate : bCompletedDate - aCompletedDate;
-            };
-        }
-        return (a: Todo, b: Todo) => (isAscending ? a.priority - b.priority : b.priority - a.priority);
     };
-
 
     const handleAdd = () => {
         router.push({
@@ -115,20 +118,87 @@ const TodoList = () => {
         setViewMode(viewMode === 'list' ? 'tabbed' : 'list');
     };
 
-    const filteredTodos = selectedTab === 'incomplete' ? todos.filter((todo) => !todo.completed) : todos.filter((todo) => todo.completed);
+    const groupTodosBy = (todos: Todo[], format: string): { title: string; data: Todo[] }[] => {
+        const grouped = todos.reduce((acc: Record<string, Todo[]>, todo) => {
+            const key = moment(todo.assignedDate).format(format);
+            acc[key] = acc[key] || [];
+            acc[key].push(todo);
+            return acc;
+        }, {});
+
+        return Object.entries(grouped).map(([title, data]) => ({ title, data }));
+    };
+
+    const groupTodos = () => {
+        const filteredTodos = viewMode === 'list'
+            ? todos
+            : todos.filter(todo => selectedTab === 'incomplete' ? !todo.completed : todo.completed);
+
+        const selectedDateMoment = moment(currentDate);
+        const currentDay = selectedDateMoment.format('YYYY-MM-DD');
+        const currentMonth = selectedDateMoment.format('YYYY-MM');
+        const currentYear = selectedDateMoment.format('YYYY');
+
+        const filteredByDate = filteredTodos.filter(todo => {
+            if (dateOption === myDate.ByDay) {
+                return moment(todo.assignedDate).format('YYYY-MM-DD') === currentDay;
+            } else if (dateOption === myDate.ByMonth) {
+                return moment(todo.assignedDate).format('YYYY-MM') === currentMonth;
+            } else if (dateOption === myDate.ByYear) {
+                return moment(todo.assignedDate).format('YYYY') === currentYear;
+            }
+            return true;
+        });
+
+        const sortedTodos = [...filteredByDate].sort(getSortFunction(sortOption));
+
+        switch (dateOption) {
+            case myDate.ByDay:
+                return [{
+                    title: currentDay,
+                    data: sortedTodos
+                }];
+            case myDate.ByMonth:
+                return groupTodosBy(sortedTodos, 'YYYY-MM-DD');
+            case myDate.ByYear:
+                return groupTodosBy(sortedTodos, 'YYYY-MM');
+            default:
+                return sortedTodos.map(todo => ({ title: '', data: [todo] }));
+        }
+    };
+
+
+    const updateCompletionProgress = () => {
+        const allTodos = groupTodos().flatMap(group => group.data);
+        const completedTodos = allTodos.filter((todo) => todo.completed).length;
+        const totalTodosNumber = allTodos.length;
+
+        return totalTodosNumber ? (completedTodos / totalTodosNumber) * 100 : 0;
+    };
+
+    useEffect(() => {
+        setCompletionProgress(updateCompletionProgress());
+    }, [todos, currentDate, dateOption, selectedTab]);
+
+    useEffect(() => {
+        // dispatch(fetchTodos());
+    }, [dispatch]);
 
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <View style={globalStyles.rowSpaceBetween}>
-                    <TouchableOpacity onPress={goToPreviousDay} style={styles.navButton}>
+                    <TouchableOpacity onPress={() => changeDate(-1)} style={styles.navButton}>
                         {icons['keyArrowLeft']()}
                     </TouchableOpacity>
-                    <TouchableOpacity style={styles.pickDateButton} onPress={() => setDatePickerVisibility(true)}>
-                        <Text style={globalTextStyles.medium22PrimaryDark}>{currentDate.toISOString().split('T')[0]}</Text>
-                        {icons['calendarCheck']()}
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={goToNextDay} style={styles.navButton}>
+                    <View style={globalStyles.alignCenter}>
+                        <TouchableOpacity style={styles.pickDateButton} onPress={() => setDatePickerVisibility(true)}>
+                            <Text style={globalTextStyles.medium22PrimaryDark}>{moment(currentDate).format(dateOption == 0 ? 'DD MMM YYYY' : dateOption == 1 ? 'MMM YYYY' : 'yyyy')}</Text>
+                            {icons['calendarCheck']()}
+                        </TouchableOpacity>
+                        <Dropdown options={dateOptions} selectedOption={DateLabels[dateOption]} onSelect={setDateOption} style={{ width: 100, padding: 0 }} btnStyle={{ paddingVertical: 0, marginTop: 5 }} />
+                    </View>
+                    <TouchableOpacity onPress={() => changeDate(1)} style={styles.navButton}>
                         {icons['keyArrowRight']()}
                     </TouchableOpacity>
                 </View>
@@ -140,7 +210,7 @@ const TodoList = () => {
 
                 <View style={globalStyles.rowSpaceBetween}>
                     <View style={globalStyles.rowStart}>
-                        <Dropdown options={sortOptions} selectedOption={sortOption} onSelect={setSortOption} style={{ width: 140, padding: 0 }} />
+                        <Dropdown options={sortOptions} selectedOption={SortLabels[sortOption]} onSelect={setSortOption} style={{ width: 140, padding: 0 }} />
                         <TouchableOpacity style={styles.toggleButton} onPress={() => setIsAscending(!isAscending)}>
                             {icons[isAscending ? 'asc' : 'desc']()}
                         </TouchableOpacity>
@@ -167,11 +237,20 @@ const TodoList = () => {
 
             <FlatList
                 showsVerticalScrollIndicator={false}
-                data={groupTodosByDay(viewMode === 'tabbed' ? filteredTodos : todos, currentDate)}
-                renderItem={({ item }) => (
-                    <TodoItem item={item} onDelete={() => handleDelete(item.id)} viewMode={viewMode} onToggleComplete={() => handleToggleComplete(item.id)} />
-                )}
-                keyExtractor={(item) => item.id.toString()}
+                data={groupTodos()}
+                renderItem={({ item }) => {
+                    return (
+                        <View style={styles.groupContainer}>
+                            {dateOption == 0 ? null : <Text style={styles.groupTitle}>
+                                {item.title ? moment(item.title).format(dateOption == 1 ? 'DD MMM' : 'MMM') : 'No Date'}
+                            </Text>}
+                            {item.data.map((task: Todo) => (
+                                <TodoItem key={task.id} viewMode={viewMode} item={task} onDelete={() => handleDelete(task.id)} onToggleComplete={() => handleToggleComplete(task.id)} />
+                            ))}
+                        </View>
+                    );
+                }}
+                keyExtractor={(item, index) => index.toString()}
                 contentContainerStyle={styles.listContainer}
                 ListEmptyComponent={() => (
                     <View style={styles.emptyContainer}>
@@ -179,8 +258,7 @@ const TodoList = () => {
                     </View>
                 )}
             />
-
-            <DateTimePickerModal isVisible={isDatePickerVisible} mode="date" date={currentDate} onConfirm={handleDateChange} onCancel={() => setDatePickerVisibility(false)} locale="en_GB" />
+            <DateTimePickerModal display="spinner" isVisible={isDatePickerVisible} date={currentDate} mode="date" onConfirm={handleDateChange} onCancel={() => setDatePickerVisibility(false)} locale="en_GB" />
         </View>
     );
 };
@@ -194,10 +272,8 @@ const styles = StyleSheet.create({
         ...globalStyles.columnCenter,
         width: 35,
         height: 35,
-        shadowColor: colors.black,
-        shadowOffset: { width: 0, height: 3 },
-        shadowOpacity: 0.3,
-        shadowRadius: 6,
+        boxShadow: '0px 3px 6px rgba(0, 0, 0, 0.3)',
+        elevation: 5,
     },
     tabContainer: { ...globalStyles.rowSpaceBetween, marginBottom: 20 },
     tabButton: {
@@ -237,6 +313,10 @@ const styles = StyleSheet.create({
         padding: 6,
         elevation: 4,
     },
-});
+    groupContainer: {
 
-export default TodoList;
+    },
+    groupTitle: {
+
+    }
+});
--- features/todos/todosSlice.ts
+++ features/todos/todosSlice.ts
@@ -9,16 +9,17 @@ interface TodosState {
 
 const initialState: TodosState = {
   todos: [
-    { id: 1, title: 'To do 1', createdDate: '2024-12-29T08:29:33.401Z', assignedDate: '2025-01-16T10:29:33.401Z', completed: false, priority: 0, description: 'Lorem ipsum...' },
-    { id: 2, title: 'To do 2', createdDate: '2024-12-30T08:29:33.401Z', assignedDate: '2025-01-16T10:29:33.401Z', completed: true, completedDate: '2024-12-29T08:29:33.401Z', priority: 1, description: 'Lorem ipsum Lorem ipsum' },
-    { id: 3, title: 'To do 3', createdDate: '2024-12-28T08:29:33.391Z', assignedDate: '2025-01-16T10:29:33.401Z', completed: false, priority: 2, description: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum vLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum' },
-    { id: 4, title: 'To do 4', createdDate: '2024-12-28T02:29:33.401Z', assignedDate: '2025-01-16T10:29:33.401Z', completed: true, completedDate: '2025-01-01T08:29:33.401Z', priority: 1, description: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum vLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum' },
+    { id: 1, title: 'To do 1', createdDate: '2024-12-29T08:29:33.401Z', assignedDate: '2024-08-16T10:29:33.401Z', completed: false, priority: 0, description: 'Lorem ipsum...' },
+    { id: 2, title: 'To do 2', createdDate: '2024-12-30T08:29:33.401Z', assignedDate: '2025-01-13T10:29:33.401Z', completed: true, completedDate: '2024-12-29T08:29:33.401Z', priority: 1, description: 'Lorem ipsum Lorem ipsum' },
+    { id: 3, title: 'To do 3', createdDate: '2024-12-28T08:29:33.391Z', assignedDate: '2025-01-14T10:29:33.401Z', completed: false, priority: 2, description: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum vLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum' },
+    { id: 4, title: 'To do 4', createdDate: '2024-12-28T02:29:33.401Z', assignedDate: '2025-01-15T10:29:33.401Z', completed: true, completedDate: '2025-01-01T08:29:33.401Z', priority: 1, description: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum vLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum' },
     { id: 5, title: 'To do 5', createdDate: '2024-12-28T10:29:33.401Z', assignedDate: '2025-01-16T10:29:33.401Z', completed: false, priority: 0, description: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum vLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum' },
-    { id: 6, title: 'To do 6 isfdjnisfn iuashid', createdDate: '2025-01-07T02:29:33.401Z', assignedDate: '2025-01-16T10:29:33.401Z', completed: true, completedDate: '2025-01-01T08:29:33.401Z', priority: 1, description: '' },
-    { id: 7, title: 'To do 7', createdDate: '2025-01-08T10:29:33.401Z', assignedDate: '2025-01-16T10:29:33.401Z', completed: false, priority: 0, description: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum vLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum' },
-    { id: 8, title: 'To do 8', createdDate: '2025-01-08T10:29:33.401Z', assignedDate: '2025-01-16T10:29:33.401Z', completed: false, priority: 0, description: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum vLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum' },
-    { id: 9, title: 'To do 9', createdDate: '2025-01-08T10:29:33.401Z', assignedDate: '2025-01-16T10:29:33.401Z', completed: false, priority: 0, description: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum vLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum' },
-    { id: 10, title: 'To do 10', createdDate: '2025-01-08T10:29:33.401Z', assignedDate: '2025-01-16T10:29:33.401Z', completed: false, priority: 2, description: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum vLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum' },
+    { id: 6, title: 'To do 6 isfdjnisfn', createdDate: '2025-01-07T02:29:33.401Z', assignedDate: '2025-02-01T10:29:33.401Z', completed: true, completedDate: '2025-01-01T08:29:33.401Z', priority: 1, description: '' },
+    { id: 7, title: 'To do 7', createdDate: '2025-01-08T10:29:33.401Z', assignedDate: '2025-02-05T10:29:33.401Z', completed: false, priority: 0, description: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum vLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum' },
+    { id: 8, title: 'To do 8', createdDate: '2025-01-08T10:28:33.401Z', assignedDate: '2025-02-05T10:29:33.401Z', completed: false, priority: 0, description: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum vLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum' },
+    { id: 9, title: 'To do 9', createdDate: '2025-01-08T10:30:33.401Z', assignedDate: '2025-02-05T10:29:33.401Z', completed: false, priority: 0, description: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum vLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum' },
+    { id: 10, title: 'To do 10', createdDate: '2025-01-08T10:29:33.401Z', assignedDate: '2025-02-05T10:29:33.401Z', completed: false, priority: 0, description: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum vLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum' },
+    { id: 11, title: 'To do 11', createdDate: '2025-01-08T10:29:33.401Z', assignedDate: '2025-02-05T10:29:33.401Z', completed: false, priority: 2, description: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum vLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum' },
   ],
   loading: false,
   error: null,
@@ -80,12 +81,12 @@ const todosSlice = createSlice({
       state.error = action.payload;
     },
     updateTodoList(state, action: PayloadAction<Todo[]>) {
-      state.todos = action.payload; // Update the todos with the sorted list
+      state.todos = action.payload;
     },
     toggleTodoCompletion(state, action: PayloadAction<number>) {
       const todo = state.todos.find((todo) => todo.id === action.payload);
       if (todo) {
-        todo.completed = !todo.completed;  // Toggle the completion status
+        todo.completed = !todo.completed;
       }
     },
   },
